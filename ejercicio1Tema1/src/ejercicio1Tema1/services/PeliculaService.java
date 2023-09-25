88af8488465756e9b7bbbc6750cb349de1514322
pgonzalez8205 <pgonzalez8205@CEP-AULA7-PC17.andalucia.fusp.ceu>
2023-09-25 15:30:46 +0200

    Iterator hecho

--- ejercicio1Tema1/src/ejercicio1Tema1/services/PeliculaService.java
+++ ejercicio1Tema1/src/ejercicio1Tema1/services/PeliculaService.java
@@ -3,6 +3,7 @@ package ejercicio1Tema1.services;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import ejercicio1Tema1.dao.Dao;
@@ -10,23 +11,24 @@ import ejercicio1Tema1.modelo.Pelicula;
 
 public class PeliculaService {
 
-	
-	
-	
-	public List<Pelicula> consultarPeliculas() throws SQLException, PeliculaServiceException{
+	public List<Pelicula> consultarPeliculas() throws SQLException, PeliculaServiceException {
 		List<Pelicula> lista = new ArrayList<>();
-		List<Pelicula> listadef = new ArrayList<>();
+
 		OpenConnection oc = new OpenConnection();
 		Dao dao = new Dao();
 		Connection conn = null;
-		conn= oc.getConnection();
+		conn = oc.getConnection();
 		lista = dao.consultarPeliculas(conn);
-		for (Pelicula pelicula : lista) {
-			if(pelicula.getLongitud()<100) {
-				listadef.add(pelicula);
+		
+		
+			for (Iterator<Pelicula> iterator = lista.iterator(); iterator.hasNext();) {
+				Pelicula pelicula = (Pelicula) iterator.next();
+				if (pelicula.getLongitud() >= 100) {
+					iterator.remove();
+				}
 			}
-		}
-		return listadef;
+		return lista;
+
 	}
-	
+
 }
